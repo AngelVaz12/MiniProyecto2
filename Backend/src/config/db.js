de4691eefa5fe893544e098994048d79ea461da4
AngelVaz12 <frijolesfritos18@gmail.com>
2026-04-16 17:54:39 -0600

    elimino .env del repositorio

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,18 +2,19 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
     if (err) {
-        console.error('Error de conexión:', err);
+        console.error('❌ Error de conexión a MySQL:', err);
         return;
     }
-    console.log('Conectado a MySQL');
+    console.log('✅ Conectado a Railway MySQL');
 });
 
 module.exports = connection;
